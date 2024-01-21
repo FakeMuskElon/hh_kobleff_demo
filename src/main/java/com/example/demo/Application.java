c799a9ba3d7133955f4bc67f3778a542c11d225c
fake.musk.elon <fake.musk.elon@gmail.com>
2024-01-21 19:35:54 +0300

    demo

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import com.example.demo.model.Student;
+import com.example.demo.model.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student Maria = new Student(
+                    "Maria",
+                    "Petrova",
+                    "Maria@mail.ru",
+                    25
+                    );
+            studentRepository.save(Maria);
+        };
     }
 
 }
